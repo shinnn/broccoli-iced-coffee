afce6e25b2ace2cb572c77af62f0c98e35baa76c
shinnn <snnskwtnb@gmail.com>
2014-09-18 20:43:16 +0900

    use tape instead of mocha
    https://github.com/substack/tape

--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
 'use strict';
 
-var assert = require('assert');
-var fs = require('fs');
-var Q = require('q');
+var readFiles = require('read-files-promise');
+var test = require('tape');
 
-var readFile = Q.denodeify(fs.readFile);
-
-describe('broccoli-iced-coffee', function() {
-  it('should compile IcedCoffeeScript', function() {
-    return Q.all([
-      readFile('test/actual/await.js'),
-      readFile('test/expected/await.js')
-    ]).spread(function(actual, expected) {
-      assert.strictEqual(actual.toString(), expected.toString());
-    });
+test('broccoli-iced-coffee', function(t) {
+  t.plan(1);
+  
+  readFiles(['test/actual/await.js', 'test/expected/await.js'])
+  .then(function(bufs) {
+    var actual = bufs[0].toString();
+    var expected = bufs[1].toString();
+    t.equal(actual, expected, 'should compile IcedCoffeeScript');
+  }, function(err) {
+    t.error(err);
   });
 });
